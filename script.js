313ec54e1aaa9f20ff49125ce21829ddab365e6b
Jie Zhang <jiezhang@JiedeMacBook-Pro.local>
2026-06-20 21:32:05 +0800

    chore: bump cache version to v=80

--- script.js
+++ script.js
@@ -1073,7 +1073,7 @@ function initObjectModal() {
       return;
     }
 
-    const imageUrl = encodeURI(imagePath) + "?v=79";
+    const imageUrl = encodeURI(imagePath) + "?v=80";
     console.log("[object] loading image:", imageUrl);
 
     const img = document.createElement("img");
